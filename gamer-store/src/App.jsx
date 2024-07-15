606185ca3588adf311ba86c1dc3cd139915256b1
Santiago Mamani <mamanisantiago485@gmail.com>
2024-07-15 17:00:38 -0300

    empezando react

---
+++ gamer-store/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
+function App () {
+
+  return (
+    <>
+      
+      < NavBar/>
+
+      <ItemListContainer greeting="hola"/>
+   
+    </>
+  ) 
+}
+
+export default App
---
+++ gamer-store/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+   return (
+    <nav>
+       <h2>GamerSTORE</h2>
+       <a href="">data</a>
+       <CartWidget/>
+    </nav>
+   )
+}
+
+export default NavBar
